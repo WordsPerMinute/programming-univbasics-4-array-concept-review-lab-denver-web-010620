6b476a17c2b0f00bea031c945865515ca16d3089
Corey Hodge <contact@coreyhodge.net>
2019-12-28 03:52:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times { |index|
     if array[index] == value_to_find
-     return index
+      return index
+    else
+      return nil
     end
   }
 end
